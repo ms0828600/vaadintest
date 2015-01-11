503303b02af5c3146ba1474d324403db23620f17
martin <ms0828600@gmail.com>
2015-01-11 21:14:47 +0100

    Add FillDB.java class for generating test-entries

---
+++ src/main/java/at/martin/FillDB.java
@@ -0,0 +1,30 @@
+package at.martin;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import at.martin.entity.Person;
+
+public class FillDB {
+
+	public static List<Person> generatePersons() {
+		List<Person> list = new ArrayList<Person>(10);
+		String[] firstNames = { "Martin", "Mario", "Josef", "Hubert", "Johannes", 
+				"Klara", "Katharina", "Andrea", "Christine", "Michael"
+		};
+		
+		String[] familyNames = { "Maier", "Hager", "Fellner", "Tomas", "Puhl", 
+				"Resch", "Dersch", "Hanse", "Roth", "Seifert"
+		};
+		
+		for (int i = 0; i < 10; i++) {
+			Person p = new Person();
+			p.setFirstName(firstNames[i]);
+			p.setFirstName(familyNames[i]);
+			list.add(p);
+		}
+		return list;
+	}
+	
+}
